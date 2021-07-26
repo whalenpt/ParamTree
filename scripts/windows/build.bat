0ed628a6250e89449efd352b7a70968301ccdfa5
Patrick Whalen <whalenpt@gmail.com>
2021-07-26 15:04:03 -0700

    Added scripts folder for holding Windows executable code for configuring and building cmake projects.

---
+++ scripts/windows/build.bat
@@ -0,0 +1,2 @@
+cd build-mingw
+make -j4
---
+++ scripts/windows/configure.bat
@@ -0,0 +1,3 @@
+cmake -S . -B build-mingw -DCMAKE_PREFIX_PATH="C:/Qt/6.1.0/mingw81_64"^
+ -DCMAKE_CXX_COMPILER="C:/Qt/Tools/mingw810_64/bin/g++.exe"^
+ -G "Unix Makefiles"
